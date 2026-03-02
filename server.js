5534365268f95cd71b8a62ce48244e07443a1c6d
Mdetlefsen77 <Mdetlefsen77@users.noreply.github.com>
2026-03-02 15:10:10 -0300

    implementation: adding modules as a good practice such as controllers and routes modules.

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
+const route = require("./routes/index");
 const PORT = process.env.PORT || 3000;
-
+app.use("/", route);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Visit http://localhost:${PORT} in your browser`);
